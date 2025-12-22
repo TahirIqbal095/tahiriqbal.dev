2f8d28067399e94ad1ec6b92e9bc2a12bc581fad
Tahir Iqbal <shahtahir786@gmail.com>
2025-12-22 18:51:13 +0530

    feat: encrypted-text

--- components/landing/about-me.tsx
+++ components/landing/about-me.tsx
@@ -7,19 +7,27 @@ import {
 } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { aboutConfig } from "@/config/about";
+import { EncryptedText } from "../ui/encrypted-text";
 
 export default function AboutMe() {
+  const staticText = aboutConfig.description.slice(0, 180);
+  const encryptedText = aboutConfig.description.slice(
+    180,
+    aboutConfig.description.length
+  );
   return (
     <Card className="h-full leading-6">
       <CardHeader>
-        <div className="flex justify-between items-center mb-1 text-xs">
+        <div className="mb-1 flex items-center justify-between text-xs">
           <CardTitle>{aboutConfig.title}</CardTitle>
           <CardLabel>{aboutConfig.label}</CardLabel>
         </div>
         <Separator />
       </CardHeader>
       <CardContent>
-        <p className="font-medium text-primary">{aboutConfig.description}</p>
+        <p className="text-primary font-medium">
+          {staticText} <EncryptedText text={encryptedText} />
+        </p>
       </CardContent>
     </Card>
   );
