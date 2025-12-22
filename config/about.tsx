82fa7ae968de5830dc32463c22e46a2625e7e6b8
Tahir Iqbal <shahtahir786@gmail.com>
2025-12-22 13:53:18 +0530

    chore: updated the about-me description

--- config/about.tsx
+++ config/about.tsx
@@ -2,5 +2,5 @@ export const aboutConfig = {
   title: "About",
   label: "Tahir Iqbal",
   description:
-    "Hey, I'm Tahir. I engineer polished web apps with Next.js TypeScript and TailwindCss Bun and PostgreSQL. I focus on the small details to ship software that is fast, beautiful, and feels great to use.",
+    "I am Tahir Iqbal, a full-stack developer with a strong appreciation for clean, thoughtful design. I am a problem solver at heart and genuinely enjoy tackling challenging problems, whether in code or beyond.",
 };
