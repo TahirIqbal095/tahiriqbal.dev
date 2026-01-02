f7e9f41c5780b41a08bab9a7428659605ae70c33
Tahir Iqbal <shahtahir786@gmail.com>
2026-01-02 18:43:05 +0530

    small ui fixes

--- components/landing/current-status.tsx
+++ components/landing/current-status.tsx
@@ -5,12 +5,12 @@ import { statusConfig } from "@/config/status";
 
 export function CurrentStatus() {
   return (
-    <Card className=" h-full">
+    <Card className="h-full">
       <CardHeader>
-        <div className="flex justify-between items-center mb-1 text-xs text-white">
+        <div className="mb-1 flex items-center justify-between text-xs text-white">
           <CardTitle>{statusConfig.title}</CardTitle>
 
-          <div className="relative h-4 w-4 flex items-center justify-center">
+          <div className="relative flex h-4 w-4 items-center justify-center">
             <span className="signal-dot"></span>
           </div>
         </div>
@@ -18,15 +18,15 @@ export function CurrentStatus() {
       </CardHeader>
 
       <div className="mt-4">
-        <div className="flex gap-2 items-center">
+        <div className="flex items-center gap-2">
           <Folder size={14} className="text-card-foreground" />
           <h4>{statusConfig.availableFor.heading}</h4>
         </div>
-        <div className="ml-2 mt-2 flex flex-col gap-2">
+        <div className="mt-2 ml-1 flex flex-col gap-2 border-l-2 pl-4">
           {statusConfig.availableFor.jobs.map((j, idx) => (
-            <div key={idx} className="flex gap-2 items-center">
+            <div key={idx} className="flex items-center gap-2">
               <File size={14} className="text-card-foreground" />
-              <h4 className="text-muted-foreground underline hover:text-foreground cursor-pointer">
+              <h4 className="text-muted-foreground hover:text-foreground cursor-pointer underline">
                 {j}
               </h4>
             </div>
--- config/footer.tsx
+++ config/footer.tsx
@@ -7,17 +7,17 @@ export const footerData = {
     {
       name: "Github",
       href: "https://github.com/tahiriqbal095",
-      icon: <Github size={20} />,
+      icon: <Github size={18} />,
     },
     {
       name: "Linkedin",
       href: "https://linkedin.com/in/tahiriqbal095",
-      icon: <Linkedin size={20} />,
+      icon: <Linkedin size={18} />,
     },
   ],
   mail: {
     title: "Email",
     email: "shahtahir786@gmail.com",
-    icon: <Mail size={20} />,
+    icon: <Mail size={18} />,
   },
 };
